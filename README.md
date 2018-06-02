9b586d1ec01cf9dba9cc27dd1ba9e25f3ea8881f
Xavier Martínez <xavier@xceed.me>
2018-06-02 12:52:27 +0200

    :sparkles: Mocked Infinite Loading

--- README.md
+++ README.md
@@ -23,6 +23,16 @@ Run Client (ReactJS App) at http://localhost:3000
 npm start
 ```
 
+Run Storybook (UI Explorer) at http://localhost:6006
+``` shell
+npm run storybook
+```
+
+Build Storybook Static
+``` shell
+npm run build-storybook
+```
+
 Make Production Bundle
 ``` shell
 npm run prod
@@ -44,21 +54,49 @@ http://localhost:8080/oriental/sushi.jpg
 npm run test
 ```
 
+* Coverage is always on, you can check also reports in /coverage/lcov-report/index.html.
+
 * Snapshot testing is also included.
 
 * E2E tests will be done in Cypress (pending).
 
 * Sentry is enabled to track errors on development phase, add another integretion on production to track real-time error data.
 
+### Dev steps
 
-
-### Considerations
+1) Dev/Prod environtment setup.
+2) Design and structure of the components layout (cont vs pres), wireframes, etc.
+3) Storybook up & running + first stateless components. Mocked info.
+4) First tests Unit & Int. Leave Cypress ready in case I have time for E2E.
+5) App in Node/Express to render the first component. See if glamor can be SSR for CSS (rehydrate).
+6) Leave routing ready in client/server.
+7) Create a Grid/List, check infinite loading with mocked data.
+8) Add Flickr API to replace mocked data (Node API).
+9) Add Redux/Redux-thunk for fetching data in client.
+10) Master - Detail: image detail transition + info.
+11) Animations
+12) Responsive/Compatibility check.
 
 * I added React Router for routing to scale this app in the future.
 
 * Storybook is used for component development, following the Component Driven Design (CDD) mindset. Snapshots and UI testing could be added there also. This is a common ground with designers also to improve the companies visual Styleguide and use it as a UI Explorer.
 
 
+### Considerations
+
+* ESLint + AirnBnB Styleguide is used for code linting.
+
+* I added React Router for routing to scale this app in the future.
+
+* Storybook is used for component development, following the Component Driven Design (CDD) mindset. Snapshots and UI testing could be added there also. This is a common ground with designers also to improve the companies visual Styleguide and use it as a UI Explorer.
+
+## Refactoring / Improvements
+
+* Lists should be virtualized, if I had more time I would use collections/masonry from react-virtualized to load images for better performance.
+
+* Bundles could be splitted using Webpacks code-splitter (chunks).
+
+* Build process needs a bit more work. Dev environtment could be run also in Docker.
 
 ### Tech Stack
-ReactJS, NodeJS, Express, Webpack3 + DevServer, SASS, Pug(Jade).
+ReactJS, Redux, Storybook, NodeJS, Express, Webpack4, Jest, Enzyme, Cypress.
--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import glamorous from 'glamorous';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import Card from '../components/Card';
 import Text from '../components/Text';
@@ -8,12 +9,35 @@ import View from '../components/View';
 class Gallery extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      hasMoreCards: true,
+      maxPage: Math.round(this.props.images.length / 3),
+      curPage: 0,
+    };
     this.handleSelection = this.handleSelection.bind(this);
+    this.loadCards = this.loadCards.bind(this);
+    this.shouldPageLoad = this.shouldPageLoad.bind(this);
   }
 
   handleSelection() {
 
+  }
+
+  loadCards(page) {
+    if (this.shouldPageLoad(page)) {
+      this.setState(() => ({
+        curPage: this.state.curPage + 1,
+      }));
+    }
+  }
+
+  shouldPageLoad(page) {
+    if (page === this.state.maxPage) {
+      this.setState(() => ({
+        hasMoreCards: false,
+      }), () => false);
+    }
+    return true;
   }
 
   render() {
@@ -31,21 +55,30 @@ class Gallery extends React.Component {
     };
     return (
       <View container justify="flex-start" style={{ flexFlow: 'row wrap' }}>
-        {this.props.images && this.props.images.length > 0 && this.props.images.map(photo => (
-          <Card
-            key={photo.id}
-            cover={`./assets/img/oriental/${photo.img}.jpg`}
-            color="white"
-            onClick={() => this.handleSelection(photo.id)}
-            clickable
-            zoom
-            dimensions={dimensions}
-            imgHeight={imgHeights}
+        {this.props.images && this.props.images.length > 0 &&
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={() => this.loadCards(this.state.curPage)}
+            hasMore={this.state.hasMoreCards}
+            loader={<div className="loader" key={0}>Loading ...</div>}
           >
-            <Text type="h2">{photo.title}</Text>
-            <Text type="p1">{photo.text}</Text>
-          </Card>
-        ))}
+            {this.props.images.slice(0, this.state.curPage * 3).map(photo => (
+              <Card
+                key={photo.id}
+                cover={`./assets/img/oriental/${photo.img}.jpg`}
+                color="white"
+                onClick={() => this.handleSelection(photo.id)}
+                clickable
+                zoom
+                dimensions={dimensions}
+                imgHeight={imgHeights}
+              >
+                <Text type="h2">{photo.title}</Text>
+                <Text type="p1">{photo.text}</Text>
+              </Card>
+            ))}
+          </InfiniteScroll>
+        }
         {(!this.props.images || this.props.images.length === 0) &&
           <Text type="p1">No images in this gallery :(</Text>
         }
